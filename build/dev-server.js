a1a6184d48906f6782f80076f446b57642c2ef59
yiyi1026 <vivian.li.uibe@gmail.com>
2017-08-04 12:14:12 -0700

    fix static path issue for dev run

--- build/dev-server.js
+++ build/dev-server.js
@@ -60,7 +60,7 @@ app.use(devMiddleware)
 app.use(hotMiddleware)
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
+var staticPath = path.posix.join(config.dev.assetsPublicPath + config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
 var uri = 'http://localhost:' + port
